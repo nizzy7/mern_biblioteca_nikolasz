4c53be599dd122322ca4ba47d3e387d41d59a005
nizzy7 <nikolaszwi@gmail.com>
2025-08-07 11:50:28 -0300

    ajuste

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
 import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
+
+export async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to the database");
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+    process.exit(1);
+  }
+}
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
+  connectToDatabase();
 });
+
 
 //senha nik08301010!
